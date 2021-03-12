5f4af4be026962637aca0dc6540c3a9edcc54c8a
carlosPlata <carlos@processtempo.com>
2021-03-12 01:49:16 -0600

    cors

--- app.js
+++ app.js
@@ -8,10 +8,20 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
 router.defineroutes(app);
 
 const server = require('http').Server(app);
-const PORT = 3000;
+const PORT = 4000;
 
 server.listen(PORT);
 console.log(`server started on port ${PORT}`);
